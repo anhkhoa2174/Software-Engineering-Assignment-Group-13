dc2e54d8be62fbecb0ecbc881367cd6b3c50e03c
DESKTOP-TSQE45N\tiend <dat.tran0912@hcmut.edu.vn>
2024-11-21 15:33:59 +0700

    Restore file

--- Assignment/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import Dashboard from '~/pages/Dashboard/Dashboard';
-import SPSOLogin from '~/pages/SPSOLogin/SPSOLogin';
-
-const routes = [
-    { path: '/', component: Dashboard },
-    { path: '/SPSO-login', component: SPSOLogin}
-];
-
-export { routes };
